42221c7b858951376ba59385fa42cac11d542fdd
Michael Jones <mjones@wetafx.co.nz>
2012-01-19 08:01:03 +1300

    Add another attribute and some annotations
    We write the annotations in rst for the moment.

--- plugin/script/sphinxexampleae.py
+++ plugin/script/sphinxexampleae.py
@@ -1,10 +1,28 @@
 
+float_attr_help = """
+This is the *annotation* for the floatAttr attribute
+
+Here are some bullet points pertaining to this attribute
+
+- The help is written in rst
+- I don't know what else to put in the list
+
+"""
+
+string_attr_help = """
+This is the *annotation* for the stringAttr attribute
+"""
+
 def process( node_name, handle ):
 
     handle.editorTemplate( beginScrollLayout=True )
 
     handle.editorTemplate( beginLayout="Float Attributes" )
-    handle.editorTemplate( "floatAttr", addControl=True )
+    handle.editorTemplate( "floatAttr", addControl=True, annotation=float_attr_help )
+    handle.editorTemplate( endLayout=True )
+
+    handle.editorTemplate( beginLayout="String Attributes" )
+    handle.editorTemplate( "stringAttr", addControl=True, annotation=string_attr_help )
     handle.editorTemplate( endLayout=True )
 
     handle.editorTemplate( addExtraControls=True )
