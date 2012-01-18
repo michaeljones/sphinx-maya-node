80ceecd896f1d110729ee55eec7bea1a753c717f
Michael Jones <mjones@wetafx.co.nz>
2012-01-19 08:02:23 +1300

    General improvements to give basic functionality
    We add the RstContentCreator to convert reStructureText annotations to
    the appropriate docutils nodes.
    
    We add the LabelConverter to do the Maya-esque attribute name to title
    conversion. ie. floatAtttr -> "Float Attr"
    
    We add a basic dispatch method as a way of responding to different
    keyword arguments in the editorTemplate call.
    
    We add the "add_control" method to handle those cases and create
    definition_list_items with the parameter name as the term and the
    parameter's annotation as the definition.
    
    Finally, we add the begin_layout & end_layout methods which add a rubric
    node with the layout's name from begin_layout and then manage the
    pushing & popping of a sublist of nodes which is then inserted into a
    definition_list once all the entries in the layout section have been
    gathered.

--- sphinxmayaae.py
+++ sphinxmayaae.py
@@ -1,8 +1,11 @@
 
 from docutils.parsers.rst import Directive
+from docutils.statemachine import ViewList
 
 import docutils.nodes
 import sphinx.addnodes
+import textwrap
+import re
 
 class NodeNotFoundError(Exception):
     pass
@@ -23,6 +26,47 @@ class NodeFactory(object):
 
         raise NodeNotFoundError(node_name)
 
+
+class RstContentCreator(object):
+
+    def __init__(self, list_type, dedent):
+
+        self.list_type = list_type
+        self.dedent = dedent
+
+    def __call__(self, text):
+
+        # Remove the first line
+        text = "\n".join(text.split(u"\n")[1:])
+
+        # Remove starting whitespace
+        text = self.dedent(text)
+
+        # Inspired by autodoc.py in Sphinx
+        result = self.list_type()
+        for line in text.split("\n"):
+            result.append(line, "<sphinxmayaae>")
+
+        return result
+
+
+
+class LabelConverter(object):
+
+    def __init__(self, expr):
+
+        self.expr = expr
+
+    def convert(self, name):
+
+        # Split on capitals
+        name = self.expr.sub(r"\1 \2", name)
+
+        # Capitalize first letter
+        name = "%s%s" % (name[0].upper(), name[1:])
+
+        return name
+
 class MayaHandle(object):
 
     def __init__(self, cmds):
@@ -36,25 +80,98 @@ class MayaHandle(object):
 
 class DocHandle(object):
 
-    def __init__( self, node_factory ):
+    def __init__(self, node_factory, label_converter, content_creator, state):
 
-        self.nodelist = []
+        self.node_list_stack = [[]]
         self.node_factory = node_factory
+        self.label_converter = label_converter
+        self.content_creator = content_creator
+        self.state = state
 
     def editorTemplate(self, *args, **kwargs):
 
+        self.dispatch("addControl", self.add_control, args, kwargs)
+        self.dispatch("beginLayout", self.begin_layout, args, kwargs)
+        self.dispatch("endLayout", self.end_layout, args, kwargs)
+
+    def dispatch(self, name, function, args, kwargs):
+
         try:
-            add_control = kwargs[ "addControl" ]
+            test = kwargs[ name ]
         except KeyError:
             return
 
-        if add_control:
-            parameter_name = args[0]
-            self.nodelist.append( self.node_factory.Text( parameter_name ) )
+        if test:
+            function(args, kwargs)
+
+    def add_control(self, args, kwargs):
+
+        parameter_name = args[0]
+        label = self.label_converter.convert(parameter_name)
+        title = self.node_factory.Text("%s (%s)" % (label, parameter_name))
+
+        try:
+            annotation = kwargs["annotation"]
+        except KeyError:
+            pass
+
+        # Parent node for the generated node subtree
+        annotation_node = self.node_factory.paragraph()
+        annotation_node.document = self.state.document
+
+        if annotation:
+
+            rst = self.content_creator(annotation)
+
+            # Generate node subtree
+            self.state.nested_parse(rst, 0, annotation_node)
+
+        self.append(
+                self.node_factory.definition_list_item(
+                    "",
+                    self.node_factory.term("", "", self.node_factory.Text(title)),
+                    self.node_factory.definition("", annotation_node)
+                    )
+                )
+
+    def begin_layout(self, args, kwargs):
+
+        layout_name = kwargs["beginLayout"]
+        self.append(
+                self.node_factory.rubric(
+                    "",
+                    "",
+                    self.node_factory.Text(layout_name)
+                    )
+                )
+
+        self.push([])
+
+    def end_layout(self, args, kwargs):
+
+        contents = self.pop()
+
+        self.append(self.node_factory.definition_list("", *contents))
+
+    def pop(self):
+
+        return self.node_list_stack.pop()
+
+    def push(self, list):
+
+        self.node_list_stack.append(list)
+
+    def append(self, node):
+
+        # Append the node to the end of the top list in the stack
+        #
+        self.node_list_stack[-1].append(node)
 
     def nodes(self):
 
-        return self.nodelist
+        assert(len(self.node_list_stack) == 1)
+
+        return self.node_list_stack[0]
 
 
 class MayaNodeDirective(Directive):
@@ -73,9 +190,16 @@ class MayaNodeDirective(Directive):
 
         function = getattr( module, function_name )
 
-        node_factory = NodeFactory( docutils.nodes, sphinx.addnodes )
-        doc_handle = DocHandle( node_factory )
-        function( "examplenode", doc_handle )
+        node_factory = NodeFactory(docutils.nodes, sphinx.addnodes)
+
+        expr = re.compile(r"([a-z0-9])([A-Z])")
+        label_converter = LabelConverter(expr)
+
+        rst_content_creator = RstContentCreator(ViewList, textwrap.dedent)
+
+        doc_handle = DocHandle(node_factory, label_converter, rst_content_creator, self.state)
+
+        function("examplenode", doc_handle)
 
         return doc_handle.nodes()
 
